e7cd05211079103236038e5dea3b6a3beb8e6c11
tsumura.yasusi <y.tsumura@cloud-capital.co.jp>
2023-07-27 11:31:36 +0900

    rename

--- Casks/google-chrome-114.rb
+++ Casks/google-chrome-114.rb
@@ -1,5 +1,5 @@
-cask "google-chrome" do
-  version "115.0.5790.102"
+cask "google-chrome-114" do
+  version "114.0.5735.198"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
